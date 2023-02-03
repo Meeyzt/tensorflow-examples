08524a8d709f1ef1554918d62264385d2742408b
Mehmet Yılmaz <64976979+Meeyzt@users.noreply.github.com>
2023-02-03 23:26:30 +0300

    update fistikci-sahap training

--- fistikci-sahap/index.js
+++ fistikci-sahap/index.js
@@ -1,13 +1,36 @@
 async function getData() {
+  const images = [];
+  const labels = [];
+
+  for (let i = 1; i < 30; i += 1) {
+    const labelArray = ['Kirmizi', 'Siirt'];
+
+    for(label of labelArray) {
+      const name = label.toLowerCase();
+      const image = await captureImage(`data/${name}/${name} ${i}.jpg`);
+      images.push(image);
+      labels.push(label);
+    }
+  }
+
+  return [images, labels];
+}
+
+async function getTestImages() {
+  const images = [];
+  const labels = [];
+
   for (let i = 1; i < 7; i += 1) {
     const labelArray = ['Kirmizi', 'Siirt'];
 
     for(label of labelArray) {
       const name = label.toLowerCase();
-      const image = captureImage(`data/${name}/${name} ${i}.jpg`);
-      console.log(image);
+      const image = await captureImage(`data/${name}/${name} (${i}).jpg`);
+      images.push(image);
+      labels.push(label);
     }
   }
+  return [images, labels];
 }
 
 function captureImage(imgUrl) {
@@ -17,9 +40,10 @@ function captureImage(imgUrl) {
   const ctx = canvas.getContext('2d');
   ctx.drawImage(image, 0, 0);
 
-  const imageData = ctx.getImageData(0, 0, 600, 600);
+  const imageData = ctx.getImageData(0, 0, 60, 60);
   const trainImage = tf.browser.fromPixels(imageData, 3);
   const trainim =  trainImage.toFloat().div(tf.scalar(127)).sub(tf.scalar(1));
+
   return trainim;
 }
 
@@ -29,7 +53,7 @@ async function showExamples(data) {
     tfvis.visor().surface({ name: 'Input Data Examples', tab: 'Input Data'});  
 
   // Get the examples
-  const examples = data.nextTestBatch(20);
+  const examples = await nextTestBatch(3);
   const numExamples = examples.xs.shape[0];
   
   // Create a canvas element to render each example
@@ -38,13 +62,14 @@ async function showExamples(data) {
       // Reshape the image to 28x28 px
       return examples.xs
         .slice([i, 0], [1, examples.xs.shape[1]])
-        .reshape([28, 28, 1]);
+        .reshape([600, 600, 3]);
     });
     
     const canvas = document.createElement('canvas');
-    canvas.width = 28;
-    canvas.height = 28;
+    canvas.width = 600;
+    canvas.height = 600;
     canvas.style = 'margin: 4px;';
+    canvas.crossOrigin = "Anonymous";
     await tf.browser.toPixels(imageTensor, canvas);
     surface.drawArea.appendChild(canvas);
 
@@ -55,9 +80,9 @@ async function showExamples(data) {
 function getModel() {
   const model = tf.sequential();
   
-  const IMAGE_WIDTH = 28;
-  const IMAGE_HEIGHT = 28;
-  const IMAGE_CHANNELS = 1;  
+  const IMAGE_WIDTH = 600;
+  const IMAGE_HEIGHT = 600;
+  const IMAGE_CHANNELS = 3;  
   
   // In the first layer of our convolutional neural network we have 
   // to specify the input shape. Then we specify some parameters for 
@@ -94,7 +119,7 @@ function getModel() {
 
   // Our last layer is a dense layer which has 10 output units, one for each
   // output class (i.e. 0, 1, 2, 3, 4, 5, 6, 7, 8, 9).
-  const NUM_OUTPUT_CLASSES = 10;
+  const NUM_OUTPUT_CLASSES = 2;
   model.add(tf.layers.dense({
     units: NUM_OUTPUT_CLASSES,
     kernelInitializer: 'varianceScaling',
@@ -116,8 +141,8 @@ function getModel() {
 
 async function train(model, data) {
   const BATCH_SIZE = 512;
-  const TRAIN_DATA_SIZE = 5500;
-  const TEST_DATA_SIZE = 1000;
+  const TRAIN_DATA_SIZE = 10;
+  const TEST_DATA_SIZE = 5;
   // Burada hangi metricleri izleyeceğimize karar veriyoruz. DEBUG Yani
   const metrics = ['loss', 'val_loss', 'acc', 'val_acc'];
   const container = {
@@ -128,19 +153,19 @@ async function train(model, data) {
   
   const fitCallbacks = tfvis.show.fitCallbacks(container, metrics);
 
+  const trainData = await nextTrainBatch(TRAIN_DATA_SIZE);
   const [trainXS, trainYs] = tf.tidy(() => {
-    const d = data.nextTrainBatch(TRAIN_DATA_SIZE);
     return [
-      d.xs.reshape([TRAIN_DATA_SIZE, 28, 28, 1]),
-      d.labels
+      trainData.xs.reshape([TRAIN_DATA_SIZE, 600, 600, 3]),
+      trainData.labels
     ];
   });
 
+  const testData = await nextTestBatch(TEST_DATA_SIZE);
   const [testXs, testYs] = tf.tidy(() => {
-    const d = data.nextTestBatch(TEST_DATA_SIZE);
     return [
-      d.xs.reshape([TEST_DATA_SIZE, 28, 28, 1]),
-      d.labels
+      testData.xs.reshape([TEST_DATA_SIZE, 600, 600, 3]),
+      testData.labels
     ];
   });
 
@@ -153,13 +178,13 @@ async function train(model, data) {
   });
 }
 
-const classNames = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+const classNames = ['Kirmizi', 'Siirt'];
 
-function doPrediction(model, data, testDataSize = 500) {
-  const IMAGE_WIDTH = 28;
-  const IMAGE_HEIGHT = 28;
-  const testData = data.nextTestBatch(testDataSize);
-  const testXs = testData.xs.reshape([testDataSize, IMAGE_WIDTH, IMAGE_HEIGHT, 1]);
+async function doPrediction(model, testDataSize = 5) {
+  const IMAGE_WIDTH = 600;
+  const IMAGE_HEIGHT = 600;
+  const testData = await nextTestBatch(testDataSize);
+  const testXs = testData.xs.reshape([testDataSize, IMAGE_WIDTH, IMAGE_HEIGHT, 3]);
   // argMax: bize ne yüksek olasılık değerinin indexini verir.
   const labels = testData.labels.argMax(-1);
   const preds = model.predict(testXs).argMax(-1);
@@ -167,7 +192,7 @@ function doPrediction(model, data, testDataSize = 500) {
   testXs.dispose();
   
   // en yüksek olasılığı tahmin olarak kullanıyoruz
-  return [preds, labels];
+  return {preds, labels};
 
   /*
     Burada herhangi bir olasılık eşiği kullanmıyoruz.
@@ -178,40 +203,135 @@ function doPrediction(model, data, testDataSize = 500) {
 }
 
 //Bir dizi tahmin ve etiketle, her sınıf için doğruluğu hesaplayabiliriz.
-async function showAccuracy(model, data) {
-  const [preds, labels] = doPrediction(model, data);
-  const classAccuracy = await tfvis.metrics.perClassAccuracy(labels, preds);
+async function showAccuracy(model) {
+  const prediction = await doPrediction(model);
+  const classAccuracy = await tfvis.metrics.perClassAccuracy(prediction.labels, prediction.preds);
   const container = { name: 'Accuracy', tab: 'Evaluation' };
   
   tfvis.show.perClassAccuracy(container, classAccuracy, classNames);
   
-  labels.dispose();
+  prediction.labels.dispose();
 }
 
-async function showConfusion(model, data) {
-  const [preds, labels] = doPrediction(model, data);
-  const confusionMatrix = await tfvis.metrics.confusionMatrix(labels, preds);
+async function showConfusion(model) {
+  const prediction = await doPrediction(model);
+  const confusionMatrix = await tfvis.metrics.confusionMatrix(prediction.labels, prediction.preds);
   const container = { name: 'Confusion Matrix', tab: 'Evaluation' };
 
-  tfvis.render.confusionMatrix(container, { values: confusionMatrix, tickLabels: classNames });
+  tfvis.render.confusionMatrix(container, { values: confusionMatrix, tickLabels: classNames });
 
-  labels.dispose();
+  prediction.labels.dispose();
 }
 
-async function run(){
-  const data = new MnistData();
+async function run() {
+  const data = await getData();
   const model = getModel();
 
-  await data.load();
-  await showExamples(data);
+  //await showExamples(data);
 
   tfvis.show.modelSummary({name: 'Model Architecture', tab: 'Model'}, model);
 
-  await train(model, data);
+  const trainedModel = await train(model, data);
   
   // Değerlendirmeyi göster
   await showAccuracy(model, data);
   await showConfusion(model, data);
 }
 
-getData();
+run();
+
+async function nextTrainBatch(batchSize) {
+  let shuffledTrainIndex = 0;
+  const trainIndices = tf.util.createShuffledIndices(100);
+  const [trainImages, trainLabels] = await getData();
+
+  return nextBatch(
+    batchSize, [trainImages, trainLabels], () => {
+      shuffledTrainIndex =
+          (shuffledTrainIndex + 1) % trainIndices.length;
+      return trainIndices[shuffledTrainIndex];
+    });
+}
+
+async function nextTestBatch(batchSize) {
+  let shuffledTestIndex = 0;
+  const testIndices = tf.util.createShuffledIndices(batchSize);
+
+  const [testImages, testLabels] = await getTestImages();
+
+  return nextBatch(batchSize, [testImages, testLabels], () => {
+    shuffledTestIndex =
+        (shuffledTestIndex + 1) % 14;
+    return testIndices[shuffledTestIndex];
+  });
+}
+
+function nextBatch(batchSize, data, index) {
+  const IMAGE_SIZE = 1080000;
+  const NUM_CLASSES = 2;
+  const batchImagesArray = new Float32Array(batchSize * IMAGE_SIZE);
+  const batchLabelsArray = new Uint8Array(batchSize * NUM_CLASSES);
+
+  for (let i = 0; i < batchSize; i++) {
+    const idx = index();
+
+    const image =
+        data[0].slice(idx * IMAGE_SIZE, idx * IMAGE_SIZE + IMAGE_SIZE);
+    batchImagesArray.set(image, i * IMAGE_SIZE);
+
+    const label =
+        data[1].slice(idx * NUM_CLASSES, idx * NUM_CLASSES + NUM_CLASSES);
+    batchLabelsArray.set(label, i * NUM_CLASSES);
+  }
+
+  const xs = tf.tensor2d(batchImagesArray, [batchSize, IMAGE_SIZE]);
+  const labels = tf.tensor2d(batchLabelsArray, [batchSize, NUM_CLASSES]);
+
+  return {xs, labels};
+}
+
+async function getModifiedMobilenet()
+{
+  const trainableLayers = ['denseModified', 'conv_pw_13_bn', 'conv_pw_13', 'conv_dw_13_bn', 'conv _dw_13'];
+
+  const mobilenet = await
+  tf.loadLayersModel('https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json');
+
+  console.log('Mobilenet model is loaded')
+
+  const x = mobilenet.getLayer('global_average_pooling2d_1');
+
+  const predictions = tf.layers.dense({units: 2,  activation: 'softmax',name: 'denseModified'}).apply(x.output);
+
+    let mobilenetModified = tf.model({inputs: mobilenet.input, outputs:  predictions, name: 'modelModified' });
+
+    console.log('Mobilenet model is modified')
+
+    mobilenetModified =
+    freezeModelLayers(trainableLayers,mobilenetModified)
+
+    console.log('ModifiedMobilenet model layers are freezed')
+
+    mobilenetModified.compile({loss: 'categoricalCrossentropy',  optimizer: tf.train.adam(1e-3), metrics:   ['accuracy','crossentropy']});
+
+    mobilenet.dispose();
+
+    return mobilenetModified;
+}
+
+function freezeModelLayers(trainableLayers,mobilenetModified)
+{
+  for (const layer of mobilenetModified.layers) 
+  {
+    layer.trainable = false;
+    for (const tobeTrained of trainableLayers) 
+    {
+      if (layer.name.indexOf(tobeTrained) === 0) 
+      {
+        layer.trainable = true;
+        break;
+      }
+    }
+  }
+  return mobilenetModified;
+}
